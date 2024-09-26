8d825665cb5b398a3794aaee68efcfd3d5e45e39
Jhonatan Baron <jhonatan.baron@uptc.edu.co>
2024-09-26 18:20:44 -0500

    ...

--- discovery-server/discovery-server.js
+++ discovery-server/discovery-server.js
@@ -11,9 +11,9 @@ app.post('/register', (req, res) => {
     if (!registeredServers.includes(serverUrl)) {
         registeredServers.push(serverUrl);
         notifyMiddlewareNewServer(serverUrl);
-        res.status(200).send({ message: 'Server registered successfully' });
+        res.status(200).send({ message: 'Servidor registrado satisfactoriamente' });
     } else {
-        res.status(400).send({ message: 'Server is already registered' });
+        res.status(400).send({ message: 'Servidor ya existe' });
     }
 });
 
@@ -21,5 +21,5 @@ app.post('/unregister', (req, res) => {
     const { serverUrl } = req.body;
     registeredServers = registeredServers.filter(url => url !== serverUrl);
     notifyMiddlewareServerRemoved(serverUrl);
-    res.status(200).send({ message: 'Server unregistered successfully' });
+    res.status(200).send({ message: 'Servidor eliminado' });
 });
