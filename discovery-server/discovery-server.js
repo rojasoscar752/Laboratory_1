b84a39adaa72c63433327c329dfe9e73bf1df82b
Jhonatan Baron <jhonatan.baron@uptc.edu.co>
2024-09-28 15:34:03 -0500

    middleware  added server and removed server

--- discovery-server/discovery-server.js
+++ discovery-server/discovery-server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
+//implementacion para registro de servidores
 let registeredServers = [];
 
 app.use(express.json());
