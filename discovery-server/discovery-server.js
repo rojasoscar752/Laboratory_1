6780b479a5e95471a759b5612c1bc53fff5e7e92
Jhonatan Baron <jhonatan.baron@uptc.edu.co>
2024-09-30 16:52:06 -0500

    Added help comments for implement Discovery server

--- discovery-server/discovery-server.js
+++ discovery-server/discovery-server.js
@@ -6,6 +6,7 @@ let registeredServers = [];
 
 app.use(express.json());
 
+//se  verifica existencia de servidor y en caso de no existir, se registra
 app.post('/register', (req, res) => {
     const { serverUrl } = req.body;
         if (!registeredServers.includes(serverUrl)) {
@@ -17,6 +18,7 @@ app.post('/register', (req, res) => {
         }
 });
 
+//eliminacion de servidor rpreviamente registrado
 app.post('/unregister', (req, res) => {
     const { serverUrl } = req.body;
     registeredServers = registeredServers.filter(url => url !== serverUrl);
@@ -24,22 +26,26 @@ app.post('/unregister', (req, res) => {
     res.status(200).send({ message: 'Servidor eliminado' });
 });
 
+//se obtienen los servidores registrados 
 app.get('/servers', (req, res) => {
     res.json({ registeredServers });
 });
 
+//Implementacion para poder notificar al middleware  de registro de server
 function notifyMiddlewareNewServer(serverUrl) {
     axios.post('http://localhost:5000/register', { newServer: serverUrl })
         .then(response => console.log('Middleware notificado con nuevo servidor', response.data))
         .catch(err => console.error('fallo al notificar middleware', err));
 }
 
+//Implementacion para poder notificar al middleware  de eliminacion de server
 function notifyMiddlewareServerRemoved(serverUrl) {
     axios.post('http://localhost:5000/unregister', { serverUrl })
         .then(response => console.log('Middleware notificado de servidor removido', response.data))
         .catch(err => console.error('fallo al notificar al middleware sobre servidor removido', err));
 }
 
+//puerto por defecto discovery server
 app.listen(3000, () => {
     console.log('Discovery Server corriendo en puerto 3000');
 });
